2c63bdc79455f8164ac0be76bf09d32073edc285
Patric Stout <truebrain@openttd.org>
2020-01-11 16:06:34 +0100

    Add: first version of 'checkout' action

---
+++ checkout/src/main.ts
@@ -0,0 +1,37 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function run(): Promise<void> {
+  const withTags = core.getInput('with_tags') || false
+  const withSubmodules = core.getInput('withSubmodules') || true
+
+  if (withTags) {
+    await exec.exec('git fetch --depth=1 origin +refs/tags/*:refs/tags/*')
+  }
+
+  if (withSubmodules) {
+    let authHeader = ''
+    await exec.exec('git config --local --get http.https://github.com/.extraheader', [], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          authHeader += data.toString()
+        }
+      }
+    })
+
+    await exec.exec('git submodule sync --recursive')
+    await exec.exec(
+      `git -c "http.extraheader=${authHeader}" -c protocol.version=2 submodule update --init --force --recursive --depth=1`
+    )
+  }
+}
+
+async function main(): Promise<void> {
+  try {
+    await run()
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
