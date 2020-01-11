14b10d121a2e4137580c23661f65326b90dcd0f7
Patric Stout <truebrain@openttd.org>
2020-01-11 16:07:47 +0100

    Add: first version of 'deploy-aws' action

---
+++ deploy-aws/src/main.ts
@@ -0,0 +1,63 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  const awsAccessKeyId = core.getInput('awsAccessKeyId', {required: true})
+  const awsSecretAccessKey = core.getInput('awsSecretAccessKey', {required: true})
+  const awsRegion = core.getInput('awsRegion', {required: true})
+  const name = core.getInput('name', {required: true})
+
+  if (!github.context.payload.deployment) {
+    throw new Error('Can only be run from within the context of GitHub Actions deployment event')
+  }
+
+  let environment
+  if (github.context.payload.deployment.environment === 'production') {
+    environment = 'Production'
+  } else {
+    environment = 'Staging'
+  }
+
+  const stack = `Live-${environment}-${name}`
+  const parameter = `/Live/Version/${environment}/${name}`
+
+  const tag = github.context.payload.deployment.payload.tag
+  const version = github.context.payload.deployment.payload.version
+  const date = github.context.payload.deployment.payload.date
+
+  const options = {
+    env: {
+      AWS_ACCESS_KEY_ID: awsAccessKeyId,
+      AWS_SECRET_ACCESS_KEY: awsSecretAccessKey
+    }
+  }
+
+  await exec.exec(
+    `aws ssm put-parameter --region ${awsRegion} --name ${parameter} --type String --overwrite --value "@${tag}" --description "${version} on ${date}"`,
+    [],
+    options
+  )
+  await exec.exec(
+    `aws cloudformation update-stack --region ${awsRegion} --stack-name ${stack} --use-previous-template`,
+    [],
+    options
+  )
+
+  core.info('Waiting for deployment to finish .. (normally ~3 minutes)')
+  await exec.exec(
+    `aws cloudformation wait stack-update-complete --region ${awsRegion} --stack-name ${stack}`,
+    [],
+    options
+  )
+}
+
+async function main(): Promise<void> {
+  try {
+    await run()
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
